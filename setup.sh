417868246e767cc0450a2ff7bdf20d3caface94f
ericfaris <ericfaris@gmail.com>
2024-01-25 16:39:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo dnf install -y git
 
 git clone https://github.com/ericfaris/server-provisioner.git
 
+sudo chmod 755 ./server-provisioner/setup.sh
+
+sudo ./server-provisioner/setup.sh
+
 sudo dnf install -y docker
 
 sudo groupadd -f docker
