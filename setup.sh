ccbbd35b9d531dd9fa87dbb3f5daa7cf3bb3e754
ericfaris <ericfaris@gmail.com>
2024-01-25 17:08:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,3 +15,4 @@ sudo systemctl enable containerd
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m) -o /usr/bin/docker-compose && sudo chmod 755 /usr/bin/docker-compose && docker-compose --version
 
 git clone https://github.com/wmnnd/nginx-certbot.git
+#see the README at https://github.com/wmnnd/nginx-certbot to understand how to NGINX and CERTBOT
